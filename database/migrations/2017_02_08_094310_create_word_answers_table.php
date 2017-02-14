323453e797b53f71e5d3cb0ed4692e2e4209ed02
VANQUYEN <hayeu_000@ubuntu.ubuntu-domain>
2017-02-14 15:30:40 +0700

    migration

---
+++ database/migrations/2017_02_08_094310_create_word_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('word_answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('word_id')->unsigned();
+            $table->integer('correct');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('word_answers');
+    }
+}
